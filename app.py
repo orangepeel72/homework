c231bbf5efa155260c16daa9fcc5c3dd3e18a5a9
neilt <21038@wc.school.nz>
2024-03-11 12:11:30 +1300

    Edited function name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlite3 import Error
 DATABASE = 'information.db'
 app = Flask(__name__)
 
-def create_con(db):
+def create_connection(db):
     try:
         connection = sqlite3.connect(db)
         return connection
@@ -15,7 +15,7 @@ def create_con(db):
 
 @app.route('/')
 def render_home():  # put application's code here
-    con = create_con(DATABASE)
+    con = create_connection(DATABASE)
     query = "SELECT students.first_name, students.last_name, work.assignment, work.class FROM allocations INNER JOIN students ON students.id=allocations.student INNER JOIN work ON work.id=allocations.assignment ORDER BY students.id"
     cur = con.cursor()
     cur.execute(query)
